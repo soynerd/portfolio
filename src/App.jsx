f1b2a7cc11d805b0117fe04c92f46c0b990f0b69
Naveen Kumar <soynerd@soynerd.soynerd>
2025-06-09 23:34:56 +0530

    removed lines

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,8 @@ function App() {
     <div className='dark:bg-gray-900'>
       <div className='max-w-4xl mx-auto '>
        <Header />
-       <div className='min-h-screen py-10'>
         <Hero />
         <Technology />
-       </div>
-
       </div>
     </div>
   )
