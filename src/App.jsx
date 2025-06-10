fdcacc81e632b50ea5b717c615ec37dd6549941a
Naveen <mesoynerd@gmail.com>
2025-06-10 22:25:08 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Hero, Technology } from './components';
+import { Header, Hero, Technology, RecentProjects, Contact } from './components';
 function App() {
   return (
     <div className='dark:bg-gray-900'>
@@ -7,6 +7,17 @@ function App() {
        <Header />
         <Hero />
         <Technology />
+        
+      </div>
+      <RecentProjects />
+      <div className='max-w-4xl mx-auto py-20 mt-20'>
+        <Contact />
+      </div>
+      <div className='max-w-4xl mx-auto text-center text-sm text-zinc-900 dark:text-zinc-100 py-2'>
+        <p>
+          © 2025 Naveen. All rights reserved. <span>Last Updated: June 10, 2025</span>
+
+        </p>
       </div>
     </div>
   )
